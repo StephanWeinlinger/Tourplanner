27c2c45a5da7c9bd088f4999e3abdefd27300ff6
StephanWeinlinger <stephan.weinlinger@gmail.com>
2022-05-25 21:28:36 +0200

    Add TourController scaffolding

---
+++ Tourplanner.Server/Controllers/TourController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tourplanner.Shared.Model;
+
+namespace Tourplanner.Server.Controllers {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TourController : ControllerBase {
+
+	    [HttpGet]
+	    public async Task<ActionResult<IEnumerable<CombinedTour>>> GetCombinedTours() {
+		    return NotFound();
+	    }
+
+	    [HttpPost]
+	    public async Task<ActionResult<Tour>> InsertTour() {
+		    return NotFound();
+	    }
+
+	    [HttpPut]
+	    public async Task<ActionResult<Tour>> UpdateTour() {
+		    return NotFound();
+	    }
+
+	    [HttpDelete]
+	    public async Task<ActionResult<Tour>> DeleteTour() {
+		    return NotFound();
+	    }
+	}
+}
