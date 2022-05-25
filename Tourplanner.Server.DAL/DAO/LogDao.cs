6b7f2e325d600001806af91d0e181e52f7c2d4d5
StephanWeinlinger <stephan.weinlinger@gmail.com>
2022-05-25 23:41:22 +0200

    Add DAO scaffolding

---
+++ Tourplanner.Server.DAL/DAO/LogDao.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tourplanner.Shared.Model;
+
+namespace Tourplanner.Server.DAL.DAO {
+	public class LogDao {
+		private const string _sqlGetAllLogsWithTourId = "SELECT * FROM \"Log\" WHERE \"TourId\" = @TourId";
+		private const string _sqlInsertLogWithTourId = "INSERT INTO \"Log\" (\"TourId\", \"Date\", \"Comment\", \"Difficulty\", \"Time\", \"Rating\") VALUES (@TourId, @Date, @Comment, @Difficulty, @Time, @Rating)";
+		private const string _sqlUpdateLog = "UPDATE \"Log\" SET \"TourId\" = @TourId, \"Date\" = @Date, \"Comment\" = @Comment, \"Difficulty\" = @Difficulty, \"Time\" = @Time, \"Rating\" = @Rating WHERE \"Id\" = @Id";
+		private const string _sqlDeleteLog = "DELETE FROM \"Log\" WHERE \"Id\" = @Id";
+
+		public IEnumerable<Log> GetAllLogsWithTourId() {
+			return null;
+		}
+
+		public Tour InsertLogWithTourId() {
+			return null;
+		}
+
+		public Tour UpdateLog() {
+			return null;
+		}
+
+		public int DeleteLog() {
+			return 0;
+		}
+	}
+}
---
+++ Tourplanner.Server.DAL/DAO/TourDao.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tourplanner.Shared.Model;
+
+namespace Tourplanner.Server.DAL.DAO {
+	public class TourDao {
+		private const string _sqlGetAllTours = "SELECT * FROM \"Tour\"";
+		private const string _sqlInsertTour = "INSERT INTO \"Tour\" (\"Name\", \"Description\", \"From\", \"To\", \"TransportType\", \"Distance\", \"Time\") VALUES (@Name, @Description, @From, @To, @TransportType, @Distance, @Time)";
+		private const string _sqlUpdateTour = "UPDATE \"Tour\" SET \"Name\" = @Name, \"Description\" = @Description, \"From\" = @From, \"To\" = @To, \"TranportType\" = @TransportType, \"Distance\" = @Distance, \"Time\" = @Time WHERE \"Id\" = @Id";
+		private const string _sqlDeleteTour = "DELETE FROM \"Tour\" WHERE \"Id\" = @Id";
+		private const string _sqlDeleteAllTours = "DELETE FROM \"Tour\"";
+
+		public IEnumerable<Tour> GetAllTours() {
+			return null;
+		}
+
+		public Tour InsertTour() {
+			return null;
+		}
+
+		public Tour UpdateTour() {
+			return null;
+		}
+
+		public int DeleteTour() {
+			return 0;
+		}
+
+		// used if import was successful (mapquest part)
+		public int DeleteAllTours() {
+			return 0;
+		}
+
+	}
+}
