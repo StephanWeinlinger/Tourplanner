fe7129be790e2ce408692bd1cfea3b29090619c6
StephanWeinlinger <stephan.weinlinger@gmail.com>
2022-06-07 15:11:34 +0200

    Add error validation base

--- Tourplanner/ViewModels/ModifyTourViewModel.cs
+++ Tourplanner/ViewModels/ModifyTourViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,23 +16,25 @@ namespace Tourplanner.Client.ViewModels {
 		public MainViewModel MainViewModel;
 		public ModifyTourView ModifyTourView;
 
-		private readonly Dictionary<string, int> _transportTypeMapping = new Dictionary<string, int> {
-			{ "Car", 0 },
-			{ "Bicycle", 1 },
-			{ "Walk", 2}
-		};
+		private readonly ErrorsViewModel _errorsViewModel;
 
 		public ModifyTourViewModel(MainViewModel mainViewModel, ModifyTourView modifyTourView) {
 			MainViewModel = mainViewModel;
 			ModifyTourView = modifyTourView;
 			ModifyTourView.DataContext = this;
+			// set default TransportType
+			_transportType = "Car";
 			// set button text
 			_buttonText = "Insert Tour";
 			// set command (insert or update)
 			CurrentCommand = new AddTourCommand(this);
+
+			_errorsViewModel = new ErrorsViewModel();
+			_errorsViewModel.ErrorsChanged += OnErrorsChanged;
 		}
 
 		public ModifyTourViewModel(MainViewModel mainViewModel, TourViewModel tourViewModel, ModifyTourView modifyTourView) {
+			_errorsViewModel = new ErrorsViewModel();
 			MainViewModel = mainViewModel;
 			ModifyTourView = modifyTourView;
 			ModifyTourView.DataContext = this;
@@ -43,9 +47,11 @@ namespace Tourplanner.Client.ViewModels {
 			_transportType = tourViewModel.TransportType;
 			// set button text and transport type index
 			_buttonText = "Update Tour";
-			_transportTypeIndex = _transportTypeMapping[tourViewModel.TransportType];
 			// set command (insert or update)
 			CurrentCommand = new UpdateTourCommand(this);
+
+			_errorsViewModel = new ErrorsViewModel();
+			_errorsViewModel.ErrorsChanged += OnErrorsChanged;
 		}
 
 		private int _id;
@@ -114,17 +120,6 @@ namespace Tourplanner.Client.ViewModels {
 			}
 		}
 
-		private int _transportTypeIndex;
-		public int TransportTypeIndex {
-			get {
-				return _transportTypeIndex;
-			}
-			set {
-				_transportTypeIndex = value;
-				OnPropertyChanged(nameof(TransportTypeIndex));
-			}
-		}
-
 		private string _buttonText;
 		public string ButtonText {
 			get {
@@ -134,6 +129,21 @@ namespace Tourplanner.Client.ViewModels {
 				_buttonText = value;
 				OnPropertyChanged(nameof(ButtonText));
 			}
+		}
+
+		public bool CanSubmit => !HasErrors;
+
+		public bool HasErrors => _errorsViewModel.HasErrors;
+
+		public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+		public IEnumerable GetErrors(string propertyName) {
+			return _errorsViewModel.GetErrors(propertyName);
+		}
+
+		private void OnErrorsChanged(object sender, DataErrorsChangedEventArgs e) {
+			ErrorsChanged?.Invoke(this, e);
+			OnPropertyChanged(nameof(CanSubmit));
 		}
 	}
 }
